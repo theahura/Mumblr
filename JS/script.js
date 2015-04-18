6ac937abdb1b4cbf5b72b46ff79a34dc2bdf777b
tyler.im <tji2105@columbia.edu>
2015-04-18 18:23:24 -0400

    fixed missing } and added more whitespace

--- JS/script.js
+++ JS/script.js
@@ -61,13 +61,17 @@ var langs =
                      ['yue-Hant-HK', '粵語 (香港)']],
  ['日本語',           ['ja-JP']],
  ['Lingua latīna',   ['la']]];
+
 for (var i = 0; i < langs.length; i++) {
   select_language.options[i] = new Option(langs[i][0], i);
 }
+
 select_language.selectedIndex = 6;
 updateCountry();
 select_dialect.selectedIndex = 6;
 showInfo('info_start');
+
+
 function updateCountry() {
   for (var i = select_dialect.options.length - 1; i >= 0; i--) {
     select_dialect.remove(i);
@@ -78,35 +82,47 @@ function updateCountry() {
   }
   select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
 }
+
+
 var create_email = false;
 var final_transcript = '';
 var recognizing = false;
 var ignore_onend;
 var start_timestamp;
+
+
 if (!('webkitSpeechRecognition' in window)) {
   upgrade();
 } else {
+
   start_button.style.display = 'inline-block';
   var recognition = new webkitSpeechRecognition();
   recognition.continuous = true;
   recognition.interimResults = true;
+
+
   recognition.onstart = function() {
     recognizing = true;
     showInfo('info_speak_now');
     start_img.src = 'mic-animate.gif';
     console.log("working!")
   };
+
+
   recognition.onerror = function(event) {
+
     if (event.error == 'no-speech') {
       start_img.src = 'mic.gif';
       showInfo('info_no_speech');
       ignore_onend = true;
     }
+
     if (event.error == 'audio-capture') {
       start_img.src = 'mic.gif';
       showInfo('info_no_microphone');
       ignore_onend = true;
     }
+
     if (event.error == 'not-allowed') {
       if (event.timeStamp - start_timestamp < 100) {
         showInfo('info_blocked');
@@ -115,17 +131,23 @@ if (!('webkitSpeechRecognition' in window)) {
       }
       ignore_onend = true;
     }
+
   };
+
+
   recognition.onend = function() {
     recognizing = false;
+
     if (ignore_onend) {
       return;
     }
+
     start_img.src = 'mic.gif';
     if (!final_transcript) {
       showInfo('info_start');
       return;
     }
+
     showInfo('');
     if (window.getSelection) {
       window.getSelection().removeAllRanges();
@@ -133,121 +155,159 @@ if (!('webkitSpeechRecognition' in window)) {
       range.selectNode(document.getElementById('final_span'));
       window.getSelection().addRange(range);
     }
+
     if (create_email) {
       create_email = false;
       createEmail();
     }
+
   };
+
+
   recognition.onresult = function(event) {
     var interim_transcript = '';
+
     for (var i = event.resultIndex; i < event.results.length; ++i) {
+
       if (event.results[i].isFinal) {
         final_transcript += event.results[i][0].transcript;
       } else {
+
         var temp_transcript = interim_transcript;
         if (!checkBadWords(temp_transcript))
         	interim_transcript += event.results[i][0].transcript;
       }
     }
+
     final_transcript = capitalize(final_transcript);
     final_span.innerHTML = linebreak(final_transcript);
     interim_span.innerHTML = linebreak(interim_transcript);
+
     if (final_transcript || interim_transcript) {
       showButtons('inline-block');
     }
  };
  
-function upgrade() {
-  start_button.style.visibility = 'hidden';
-  showInfo('info_upgrade');
-}
-var two_line = /\n\n/g;
-var one_line = /\n/g;
-function linebreak(s) {
-  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
-}
-var first_char = /\S/;
-function capitalize(s) {
-  return s.replace(first_char, function(m) { return m.toUpperCase(); });
-}
-function createEmail() {
-  var n = final_transcript.indexOf('\n');
-  if (n < 0 || n >= 80) {
-    n = 40 + final_transcript.substring(40).indexOf(' ');
-  }
-  var subject = encodeURI(final_transcript.substring(0, n));
-  var body = encodeURI(final_transcript.substring(n + 1));
-  window.location.href = 'mailto:?subject=' + subject + '&body=' + body;
-}
-function copyButton() {
-  if (recognizing) {
-    recognizing = false;
-    recognition.stop();
-  }
-  copy_button.style.display = 'none';
-  copy_info.style.display = 'inline-block';
-  showInfo('');
-}
-function emailButton() {
-  if (recognizing) {
-    create_email = true;
-    recognizing = false;
-    recognition.stop();
-  } else {
-    createEmail();
-  }
-  email_button.style.display = 'none';
-  email_info.style.display = 'inline-block';
-  showInfo('');
-}
-function startButton(event) {
-  if (recognizing) {
-    recognition.stop();
-    return;
-  }
-  final_transcript = '';
-  recognition.lang = select_dialect.value;
-  recognition.start();
-  ignore_onend = false;
-  final_span.innerHTML = '';
-  interim_span.innerHTML = '';
-  start_img.src = 'mic-slash.gif';
-  showInfo('info_allow');
-  showButtons('none');
-  start_timestamp = event.timeStamp;
-}
-function showInfo(s) {
-  if (s) {
-    for (var child = info.firstChild; child; child = child.nextSibling) {
-      if (child.style) {
-        child.style.display = child.id == s ? 'inline' : 'none';
-      }
-    }
-    info.style.visibility = 'visible';
-  } else {
-    info.style.visibility = 'hidden';
-  }
-}
-var current_style;
-function showButtons(style) {
-  if (style == current_style) {
-    return;
-  }
-  current_style = style;
-  copy_button.style.display = style;
-  email_button.style.display = style;
-  copy_info.style.display = 'none';
-  email_info.style.display = 'none';
-}
 
-function checkBadWords(transcript) {
-	var bool = false;
-	if ("uh" in transcript || "um" in transcript) {
-		//PLAY ARGH
-		bool = true;
+	function upgrade() {
+	  start_button.style.visibility = 'hidden';
+	  showInfo('info_upgrade');
 	}
-	if ("like" in transcript) {
-		bool = true;
+
+
+	var two_line = /\n\n/g;
+	var one_line = /\n/g;
+
+
+	function linebreak(s) {
+	  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
 	}
-	return bool;
+
+	var first_char = /\S/;
+
+
+	function capitalize(s) {
+	  return s.replace(first_char, function(m) { return m.toUpperCase(); });
+	}
+
+
+	function createEmail() {
+	  var n = final_transcript.indexOf('\n');
+	  if (n < 0 || n >= 80) {
+	    n = 40 + final_transcript.substring(40).indexOf(' ');
+	  }
+	  var subject = encodeURI(final_transcript.substring(0, n));
+	  var body = encodeURI(final_transcript.substring(n + 1));
+	  window.location.href = 'mailto:?subject=' + subject + '&body=' + body;
+	}
+
+
+	function copyButton() {
+	  if (recognizing) {
+	    recognizing = false;
+	    recognition.stop();
+	  }
+	  copy_button.style.display = 'none';
+	  copy_info.style.display = 'inline-block';
+	  showInfo('');
+	}
+
+
+	function emailButton() {
+	  if (recognizing) {
+	    create_email = true;
+	    recognizing = false;
+	    recognition.stop();
+	  } else {
+	    createEmail();
+	  }
+	  email_button.style.display = 'none';
+	  email_info.style.display = 'inline-block';
+	  showInfo('');
+	}
+
+
+	function startButton(event) {
+	  if (recognizing) {
+	    recognition.stop();
+	    return;
+	  }
+
+	  final_transcript = '';
+	  recognition.lang = select_dialect.value;
+	  recognition.start();
+	  ignore_onend = false;
+	  final_span.innerHTML = '';
+	  interim_span.innerHTML = '';
+	  start_img.src = 'mic-slash.gif';
+	  showInfo('info_allow');
+	  showButtons('none');
+	  start_timestamp = event.timeStamp;
+	}
+
+
+	function showInfo(s) {
+	  if (s) {
+	    for (var child = info.firstChild; child; child = child.nextSibling) {
+	      if (child.style) {
+	        child.style.display = child.id == s ? 'inline' : 'none';
+	      }
+	    }
+	    info.style.visibility = 'visible';
+	  } else {
+	    info.style.visibility = 'hidden';
+	  }
+	}
+
+	var current_style;
+
+	function showButtons(style) {
+	  
+	  if (style == current_style) {
+	    return;
+	  }
+
+	  current_style = style;
+	  copy_button.style.display = style;
+	  email_button.style.display = style;
+	  copy_info.style.display = 'none';
+	  email_info.style.display = 'none';
+
+	}
+
+
+	function checkBadWords(transcript) {
+		var bool = false;
+
+		if ("uh" in transcript || "um" in transcript) {
+			//PLAY ARGH
+			bool = true;
+		}
+
+		if ("like" in transcript) {
+			bool = true;
+		}
+
+		return bool;
+	}
 }

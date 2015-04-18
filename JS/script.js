026cd3c860f7e71d94e74746067f367ff3fc0451
Adrian Garza <adrianhgarza@gmail.com>
2015-04-18 15:37:59 -0400

    a smaller, less powerful script.js
    Signed-off-by: Adrian Garza <adrianhgarza@gmail.com>

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,12 @@
-var argh = new Audio ("argh.mp3");
+var argh = new Audio ("sound/argh.mp3");
 			
 function arghPlay() {
 	argh.play();
 	argh.currentTime = 0;
 }
 
-//init recognition things
-var recognition = new webkitSpeechRecognition();
-recognition.lang = "en-US";
 
-//main function for speech interruption
-function speechAnalysis() {
-	if ("um" in recognition) {
-		arghPlay();
-	}
-	if ("uh in recognition") {
-		arghPlay();
-	}
-	if ("like" in recognition) {
-		arghPlay();
-	}
-}
+$(".button").click(function() {
+	$("#screen1").slideUp();
+	console.log("Success?");
+})	

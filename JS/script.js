6f2d01272b13685a3cfda25228b873c0a0db66a2
Amol Kapoor <theahura@gmail.com>
2015-04-19 09:01:37 -0400

    Signed-off-by: Amol Kapoor <theahura@gmail.com>

--- JS/script.js
+++ JS/script.js
@@ -5,6 +5,9 @@ var potentialCrutch = []
 var currentTranscript = "";
 var currentIndex = 0;
 
+var currentTriggeredWord = "";
+var minimumIndicesPassed = 0;
+
 var buffer = [];
 var timeBuffer = [];
 var bufferSize = 4;
@@ -60,53 +63,56 @@ recognition.onend = function() {
 };
 
 
-recognition.onresult = function(event) {
-
-  var transcript =  event.results[0][0].transcript;
+recognition.onresult = function(event) 
+{
+   var transcript =  event.results[event.results.length - 1][0].transcript;
 
   //checks the transcript
-  if(currentTranscript !== transcript && event.results[0][0].confidence > .75)
+  if(currentTranscript !== transcript && event.results[event.results.length - 1][0].confidence > .75)
   {
     currentTranscript = transcript;
 
+    console.log(transcript)
+
     var transcriptArray = transcript.split(" ");
+    
+    if(minimumIndicesPassed > 0)
+      minimumIndicesPassed--
 
-    var shortenedArray = transcriptArray;
-
-    shortenedArray.splice(0, i);
-
-    console.log(shortenedArray);
-
-    for(var i = 0; i < shortenedArray.length; i++)
+    for(var j = 4; j > 0; j--)
     {
-      if(i > bufferSize)
-        break
-
-      buffer.push(shortenedArray[i])
+      if(j < transcriptArray.length)
+      {
+        buffer.push(transcriptArray[transcriptArray.length - j].toLowerCase())
+      }
 
       if(buffer.length > bufferSize)
         buffer.splice(0, 1)
     }
 
+    console.log(buffer)
+ 
     if (checkAbsoluteCrutchWords(buffer)) 
     {
       arghPlay();
     }
     else if(checkPotentialCrutchWords(buffer))
     {
+      console.log(weight);
       weight += 0.025;
       var bufString = buffer.join(" ");
       checkNGram(bufString);
       waitingForResponses++;
+      console.log(waitingForResponses);
     }
     else
     {
-      currentIndex = transcriptArray.length - 1;
-
       if(waitingForResponses == 0)
         weight = 0.0;
     }  
   }
+
+ 
 }
 
 
@@ -134,7 +140,16 @@ function checkAbsoluteCrutchWords(buffer)
   {
     if(absoluteCrutch.indexOf(buffer[i]) != -1)
     {
-      return true;
+      if(currentTriggeredWord != buffer[i] || minimumIndicesPassed == 0)
+      {
+        console.log(buffer[i])
+        console.log(minimumIndicesPassed)
+        currentTriggeredWord = buffer[i];
+        minimumIndicesPassed = bufferSize;
+        console.log("crutch found");
+        return true;
+      }
+
     }
   }
 
@@ -143,6 +158,7 @@ function checkAbsoluteCrutchWords(buffer)
 
 function checkPotentialCrutchWords(buffer)
 {
+  console.log(buffer)
   for(var i = 0; i < buffer.length; i++)
   {
     if(potentialCrutch.indexOf(buffer[i]) != -1)

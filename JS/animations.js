7133baf1b322f97d3b518e7d1a061d30dbdc9168
Adrian Garza <adrianhgarza@gmail.com>
2015-04-19 06:50:25 -0400

    Signed-off-by: Adrian Garza <adrianhgarza@gmail.com>

--- JS/animations.js
+++ JS/animations.js
@@ -1,22 +1,38 @@
-$(document).ready(function(){	
-	
-	//slides everythng under "screen1" up
-	$("#upbutton").click(function() {
-		$("#screen1").slideUp();
-		localStorage.crutchWords = document.getElementById("crutchWords").innerHTML;
-		console.log("ayy lmao?"); //I should take this out at some point
-	});
+function returnArray(id) {
 
-	//slides everythin under "screen1" down
-	$("#downbutton").click(function() {
-		$("#screen1").slideDown();
-	});
+	//get the VALUE of an html text object (not the object itself)
+	//use javascript string.split(" ") to return an array created from the word split by spaces
+	var str = $("#"+id).html();
+	console.log(str);
+	var res = str.split(" ");
+	console.log(res);
+	return res;
+}
 
-	//so proud of this light-up animation
-	$(".header").hover(function() {
-		$(".header").css("background-color", "#84CB99");
-		}, function() {
-		$(".header").css("background-color", "#76C58E");	
-	});	
+//slides everythng under "screen1" up
+$("#upbutton").click(function() {
+	$("#screen1").slideUp();
 
-})
+	//stores the field contents locally
+	localStorage.crutchWords = document.getElementById("crutchWords").innerHTML;
+	localStorage.sometimesCrutchWords = document.getElementById("sometimesCrutchWords").innerHTML;
+
+	//loop that updates the two arrays
+	returnArray("crutchWords");
+	returnArray("sometimesCrutchWords");
+
+	console.log("ayy lmao?"); //I should take this out at some point
+});
+
+//slides everythin under "screen1" down
+$("#downbutton").click(function() {
+	$("#screen1").slideDown();
+});
+
+//so proud of this light-up animation
+$(".header").hover(function() {
+	$(".header").css("background-color", "#84CB99");
+	}, function() {
+	$(".header").css("background-color", "#76C58E");	
+});	
+

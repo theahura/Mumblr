eb2204fe6c05dd7534e3a3c8343d1e88b886183f
Adrian Garza <adrianhgarza@gmail.com>
2015-04-19 11:36:36 -0400

    Signed-off-by: Adrian Garza <adrianhgarza@gmail.com>

--- JS/sound.js
+++ JS/sound.js
@@ -1,8 +1,16 @@
 var arghRandom = new Audio("argh.mp3")
-function arghRandom() {
-  var arghs = ["argh.mp3", "argh1.mp3", "argh2.mp3", "argh3.mp3", "argh4.mp3", "argh5.mp3", "argh6.mp3", "argh7.mp3", "argh8.mp3"];
-  var rand = arghs[Math.floor(Math.random() * arghs.length)];
-  var arghRandom = arghs[rand];
-  arghRandom.play();
-  arghRandom.currentTime = 0;
+// function playArghRandom() 
+// {
+//   var arghs = ["argh.mp3", "argh1.mp3", "argh2.mp3", "argh3.mp3", "argh4.mp3", "argh5.mp3", "argh6.mp3", "argh7.mp3", "argh8.mp3"];
+//   var rand = Math.floor(Math.random() * arghs.length);
+//   arghRandom = new Audio(arghs[rand]);
+//   arghRandom.play();
+//   arghRandom.currentTime = 0;
+// }
+
+function arghPlay()
+{
+	arghRandom.play()
+	waitingForResponses = 0;
+    weight = 0.0;
 }

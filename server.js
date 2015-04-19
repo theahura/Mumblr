738ac3c984d78771ff99b6d895c5e51759553293
Amol Kapoor <theahura@gmail.com>
2015-04-19 02:41:10 -0400

    server is done; adding server handler base file
    Signed-off-by: Amol Kapoor <theahura@gmail.com>

--- server.js
+++ server.js
@@ -1,113 +1,94 @@
 
-function Tree(inputValue)
+
+var fourTree = {}
+var threeTree = {}
+
+function doesNGramExist(tree, ngram)
 {
-	var children = {}
-	var value = "base";
+	if(ngram.length <= 0)
+		return true
 
-	if(inputValue)
-		value = inputValue
-
-
-	this.getValue = function()
+	var nextNode = tree[ngram[0]]
+	if(nextNode)
 	{
-		return value
+		ngram.splice(0, 1)
+		return doesNGramExist(nextNode, ngram)
+	}
+	return false
+}
+
+function printTree(tree, tab)
+{
+	var spaces = ""
+	for(var i = 0; i < tab; i++)
+	{
+		spaces = spaces + "---"
 	}
 
-	this.findChildTree = function(inputString)
+	for(i in tree)
 	{
-		return children[inputString]
-	}
-
-	this.push = function(childValue)
-	{
-		children[childValue] = new Tree(childValue)
-		return children[childValue]
-	}
-
-	this.doesNGramExist = function(ngram)
-	{
-		if(ngram.length <= 0)
-			return true
-
-		var nextNode = this.findChildTree(ngram[0])
-		if(nextNode)
-		{
-			ngram.splice(0, 1)
-			return nextNode.doesNGramExist(ngram)
-		}
-		return false
-	}
-
-	this.createTree = function(data, n)
-	{
-		n = n+1
-		for(var i = 0; i < data.length; i += n)
-		{
-			//console.log(i + " " + data[i])
-			var currentTree = this
-			for(var j = 1; j < n; j++)
-			{
-				var childTree = currentTree.findChildTree(data[i+j])
-				if(childTree)
-				{
-					currentTree = childTree
-				}
-				else
-				{
-					currentTree = currentTree.push(data[i+j])
-				}
-			}
-		}
-	}
-
-	this.print = function(tab)
-	{
-		var spaces = ""
-		for(var i = 0; i < tab; i++)
-		{
-			spaces = spaces + "---"
-		}
-
-		for(i in children)
-		{
-			console.log(":" + spaces + children[i].getValue())
-			children[i].print(tab + 1)
-		}
+		console.log(":" + spaces + i)
+		printTree(tree[i], tab + 1)
 	}
 }
 
-var fiveTree = new Tree();
-var fourTree = new Tree();
-var threeTree = new Tree();
-
 //opens folder, creates tree
 fs = require('fs')
-
-fs.readFile('w5_.txt', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var temparray = data.split(/[\t\r\n]+/)
-  fiveTree.createTree(temparray, 5)
-});
 
 fs.readFile('w4_.txt', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  var temparray = data.split(/[\t\r\n]+/)
-  fiveTree.createTree(temparray, 4)
+  console.log("TREE BUILDING FOUR STARTED");
+  var temparray = data.split(/[\t\r\n]+/);
+
+  for (var i = 0; i < temparray.length; i += 5) 
+  {
+    var currTree = fourTree;
+
+    for (var j = 1; j < 5; j++) {
+        if (temparray[i + j] in currTree) {
+            currTree = currTree[temparray[i + j]];
+        } else {
+            currTree[temparray[i + j]] = {};
+            currTree = currTree[temparray[i + j]];
+        }
+        //temparray[i+j] = undefined;
+    }
+  }
+
+  console.log("TREE BUILDING FOUR COMPLETE");
+  console.log(doesNGramExist(fourTree, ["one", "of", "the", "loudest"]));
 });
 
 fs.readFile('w3_.txt', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  var temparray = data.split(/[\t\r\n]+/)
-  fiveTree.createTree(temparray, 3)
+  console.log("TREE BUILDING THREE STARTED");
+  var temparray = data.split(/[\t\r\n]+/);
+  
+  for (var i = 0; i < temparray.length; i += 4) 
+  {
+    var currTree = threeTree;
+
+    for (var j = 1; j < 4; j++) {
+        if (temparray[i + j] in currTree) {
+            currTree = currTree[temparray[i + j]];
+        } else {
+            currTree[temparray[i + j]] = {};
+            currTree = currTree[temparray[i + j]];
+        }
+        //temparray[i+j] = undefined;
+    }
+  }
+
+  console.log("TREE BUILDING THREE COMPLETE");
+
+  console.log(doesNGramExist(threeTree, ["i", "love", "you"]));
 });
 
-
+//starts not tree building stuff
 var io = require('socket.io').listen(3004);
 
 //On an io socket connection...
@@ -116,9 +97,9 @@ io.sockets.on('connection', function(socket)
 	socket.on('clientToServer', function(data)
 	{
 		if(!(data && data.name))
-			serverError(socket, "Data did not have a name")
+			serverError(socket, "Data did not have a name");
 
-		serverHandler(socket, data)
+		serverHandler(socket, data);
 	});
 
 });
@@ -136,16 +117,16 @@ function serverHandler(socket, incomingObj)
 	if(incomingObj.name == "checkNGram")
 	{
 		if(!incomingObj.ngram)
-			serverError(socket, "No ngram found")
+			serverError(socket, "No ngram found");
 
 		if(incomingObj.ngram.length == 3)
-			checkThreeTree(socket, incomingObj.ngram)
+			checkThreeTree(socket, incomingObj.ngram);
 		else if (incomingObj.ngram.length == 4)
-			checkFourTree(socket, incomingObj.ngram)
+			checkFourTree(socket, incomingObj.ngram);
 		else if (incomingObj.ngram.length == 5)
-			checkFiveTree(socket, incomingObj.ngram)
+			checkFiveTree(socket, incomingObj.ngram);
 		else
-			serverError(socket, "ngram not the right length <3, 4, 5>")
+			serverError(socket, "ngram not the right length <3, 4, 5>");
 	}
 }
 
@@ -156,14 +137,14 @@ function checkThreeTree(socket, data)
 		socket.emit('ServerToClient',{
 			name: "NGramResponse",
 			value: true
-		})	
+		});	
 	}
 	else
 	{
 		socket.emit('ServerToClient',{
 			name: "NGramResponse",
 			value: false
-		})
+		});
 	}
 }
 
@@ -174,14 +155,14 @@ function checkFourTree(socket, data)
 		socket.emit('ServerToClient',{
 			name: "NGramResponse",
 			value: true
-		})	
+		});
 	}
 	else
 	{
 		socket.emit('ServerToClient',{
 			name: "NGramResponse",
 			value: false
-		})
+		});
 	}
 }
 
@@ -192,13 +173,13 @@ function checkFiveTree(socket, data)
 		socket.emit('ServerToClient',{
 			name: "NGramResponse",
 			value: true
-		})	
+		});	
 	}
 	else
 	{
 		socket.emit('ServerToClient',{
 			name: "NGramResponse",
 			value: false
-		})
+		});
 	}
 }

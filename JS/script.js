fa618aa0595087166c75c9a4b7948b49c522988b
tyler.im <tji2105@columbia.edu>
2015-04-19 02:35:14 -0400

    messed around with some shit & formatting

--- JS/script.js
+++ JS/script.js
@@ -1,104 +1,108 @@
 
+var phrase = ['', '', '', ''];
+
+var recognition = new webkitSpeechRecognition();
+
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.maxAlternatives = 0;
 var recognizing = false;
 var ignore_onend;
 var start_timestamp;
 
-var phrase = ['', '', '', '', ''];
-
-  var recognition = new webkitSpeechRecognition();
-  recognition.continuous = true;
-  recognition.interimResults = true;
-  recognition.maxAlternatives = 0;
-  console.log("working");
-
-  recognition.onstart = function() {
-    recognizing = true;
-    console.log("Speak now");
-    console.log("working!")
-  };
+recognition.onstart = function() {
+  recognizing = true;
+};
 
 
-  recognition.onerror = function(event) {
+recognition.onerror = function(event) {
 
-    if (event.error == 'no-speech') {
-      console.log("No speech detected");
-      ignore_onend = true;
-    }
-
-    if (event.error == 'audio-capture') {
-      console.log("There appears to be no microphone");
-      ignore_onend = true;
-    }
-
-    if (event.error == 'not-allowed') {
-      if (event.timeStamp - start_timestamp < 100) {
-        console.log("Your shit is blocked dude");
-      } else {
-        console.log("You've been denied access dude");
-      }
-      ignore_onend = true;
-    }
-
-  };
-
-
-  recognition.onend = function() {
-    recognizing = false;
-
-    if (ignore_onend) {
-      return;
-    }
-
-    if (false) {
-      console.log("START");
-      return;
-    }
-
-  };
-
-
-  recognition.onresult = function(event) {
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-      console.log(event.results[0][i].item);
-      phrase.push(event.results[0][i]);
-      phrase.splice(0, 1);
-      console.log(phrase);
-    }
+  if (event.error == 'no-speech') {
+    console.log("No speech detected");
+    ignore_onend = true;
   }
 
+  if (event.error == 'audio-capture') {
+    console.log("There appears to be no microphone");
+    ignore_onend = true;
+  }
 
-	function startButton() {
-	  if (recognizing) {
-	    recognition.stop();
-	    return;
-	  }
-    interim_transcript = '';
-	  recognition.start();
-	  ignore_onend = false;
-	  console.log("Allow");
-	  start_timestamp = event.timeStamp;
+  if (event.error == 'not-allowed') {
+    if (event.timeStamp - start_timestamp < 100) {
+      console.log("Your shit is blocked dude");
+    } else {
+      console.log("You've been denied access dude");
+    }
+    ignore_onend = true;
+  }
 
-	}
+};
 
-	var argh = new Audio ("argh.mp3");
-			
-	function arghPlay() {
-		argh.play();
-		argh.currentTime = 0;
-	}
 
-	function checkCrutchWords(transcript) {
-	  var bool = false;
+recognition.onend = function() {
 
-	  if (transcript.indexOf("uh") > -1 || transcript.indexOf("um") > -1) {
-	    arghPlay();
-	    bool = true;
-	  }
+  recognizing = false;
+  if (ignore_onend) {
+    return;
+  }
 
-	  if (transcript.indexOf("like") > -1) {
-	    arghPlay();
-	    bool = true;
-	  }
+};
 
-	  return bool;
-	}
+
+recognition.onresult = function(event) {
+
+  for (var i = event.resultIndex; i < event.results.length; ++i) {
+
+    console.log(event.results[0][i].transcript);
+    phrase.push(event.results[0][i].transcript);
+    phrase.splice(0, 1);
+    console.log(phrase);
+
+    for (word in phrase) {
+
+      if (checkCrutchWords(word)) {
+        arghPlay();
+        phrase.remove(phrase.indexOf(word));
+        recognition.stop();
+        recognition.start();
+      }
+    }
+
+  }
+}
+
+
+function startButton() {
+  if (recognizing) {
+    recognition.stop();
+    return;
+  }
+  recognition.start();
+  ignore_onend = false;
+  console.log("Start!");
+  start_timestamp = event.timeStamp;
+
+}
+
+var argh = new Audio ("argh.mp3");
+		
+function arghPlay() {
+	argh.play();
+	argh.currentTime = 0;
+}
+
+function checkCrutchWords(transcript) {
+  var bool = false;
+
+  if (transcript.indexOf("uh") > -1 || transcript.indexOf("um") > -1) {
+    arghPlay();
+    bool = true;
+  }
+
+  if (transcript.indexOf("like") > -1) {
+    arghPlay();
+    bool = true;
+  }
+
+  return bool;
+}
